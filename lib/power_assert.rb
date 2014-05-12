87dbeec2740f5e3ee6ca07219e3d6521211b7449
todesking <discommunicative@gmail.com>
2014-05-13 00:55:19 +0900

    extract from ast

--- lib/power_assert.rb
+++ lib/power_assert.rb
@@ -3,4 +3,52 @@ require 'patm'
 require 'typedocs'
 
 module PowerAssert
+  class Extractor
+    include Typedocs::DSL
+
+    tdoc.typedef :@AST, 'Array'
+    tdoc.typedef :@Bool, 'TrueClass|FalseClass'
+
+    tdoc 'String -> Numeric -> @AST'
+    def extract(path, line_no)
+      whole_ast = Ripper.sexp(File.read(path))
+      extract_from_ast(whole_ast, line_no)
+    end
+
+    tdoc '@AST -> Numeric -> ?@AST|nil -> @AST|nil'
+    def extract_from_ast(ast, line_no, block_ast = nil)
+      p = ::Patm
+      _1 = p._1
+      _2 = p._2
+      __ = p._any
+      case ast
+      when m = p.match([p.or(:do_block, :brace_block), p._any, p._any[:exprs]])
+        m[:exprs].each do|expr|
+          found = extract_from_ast(expr, line_no, ast)
+          return found if found
+        end
+      when m = p.match([:binary, _1, __, _2])
+        found =extract_from_ast(m._1, line_no, block_ast) ||
+          extract_from_ast(m._2, line_no, block_ast)
+        return found if found
+      when m = p.match([:@int, __, [_1, __]])
+        return block_ast if block_ast && m._1 == line_no
+      else
+        if ast.is_a?(Array) && ast.size > 0
+          exprs =
+            if ast[0].is_a?(Symbol) # some struct
+              ast[1..-1]
+            else
+              ast
+            end
+          exprs.each do|expr|
+            next unless expr.is_a?(Array)
+            found = extract_from_ast(expr, line_no, nil)
+            return found if found
+          end
+        end
+      end
+      nil
+    end
+  end
 end
--- spec/power_assert_spec.rb
+++ spec/power_assert_spec.rb
@@ -1,4 +1,18 @@
+require 'typedocs/enable'
 require File.join(File.dirname(__FILE__), '..', 'lib', 'power_assert.rb')
 
-describe PowerAssert do
+describe PowerAssert::Extractor do
+  it do
+    ast = subject.extract_from_ast(Ripper.sexp(<<-RUBY), 3)
+      describe "hogehoge" do
+        it { a.b.c == d }
+        it { 1 + 2 == 3 }
+      end
+    RUBY
+    ast.should_not be_nil
+    ast[0].should == :brace_block
+    ast[2][0][0].should == :binary
+    ast[2][0][2].should == :==
+    ast[2][0][3].should == [:@int, "3", [3, 22]]
+  end
 end
